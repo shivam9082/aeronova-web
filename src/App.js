0664db6b10d5f384bd93050317bedffa1713c3c2
Shivam Pandey <shpandey9082@gmail.com>
2026-01-27 20:42:55 +0530

    initial setup for the frontend

---
+++ src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-base-200">
+      <button className="btn btn-primary">Hello DaisyUI!</button>
+    </div>
+  );
+}
+
+export default App;
